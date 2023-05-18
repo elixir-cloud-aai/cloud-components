14896f46de28f1ed720b0b3437f82bd374f6da5e
Jae Aeich <javedh@iitbhilai.ac.in>
2023-05-18 11:23:47 +0530

    chore: custom eslint example usage

---
+++ apps/example-usage/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	root: true,
+	// global rules will be exported from the eslint package elixir
+	extends: ["elixir"],
+	// rules can be overwritten or defined here
+	rules: {
+		"lines-around-directive": "off",
+	},
+};
