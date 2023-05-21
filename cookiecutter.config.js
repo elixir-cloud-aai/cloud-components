5faf369eb021cbe163e4a4e23e72a12387d4d9dd
Anurag Gupta <anurag@Anurags-MacBook-Pro.local>
2023-05-21 23:03:15 +0530

    chore: adding PACKAGE templates

---
+++ cookiecutter.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    name: "WEB COMPONENT PACKAGE",
+    templatePath: "templates/PACKAGE_NAME",
+    outputPath: "packages/",
+    fields: [
+      {
+        templateVariable: "PACKAGE_NAME",
+        question: "What is the package's name?",
+      },
+    ],
+  },
+];
