81bdf4bd25529b9071a7b4a59eb1aaf9387be68e
Anurag Gupta <anurag@Anurags-MacBook-Pro.local>
2023-05-24 16:22:38 +0530

    fix(global): fixed ts linting rules

--- packages/eslint-config-elixir/index.js
+++ packages/eslint-config-elixir/index.js
@@ -1,4 +1,14 @@
 module.exports = {
-  extends: ["turbo", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['turbo', 'airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
   rules: {},
 };
