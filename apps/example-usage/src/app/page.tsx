9c17e99029e6d7e6345e95b92c7b59001a01e5b7
Anurag Gupta <anurag@Anurags-MacBook-Pro.local>
2023-05-27 13:05:31 +0530

    ci(checks): assign test to another job

--- apps/example-usage/src/app/page.tsx
+++ apps/example-usage/src/app/page.tsx
@@ -2,6 +2,20 @@
 
 // eslint-disable-next-line import/no-unresolved
 import 'elixir-ui';
+import * as React from 'react';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  namespace JSX {
+    // eslint-disable-next-line no-unused-vars
+    interface IntrinsicElements {
+      'fast-welcome': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
 
 export default function Home() {
   return <fast-welcome></fast-welcome>;
