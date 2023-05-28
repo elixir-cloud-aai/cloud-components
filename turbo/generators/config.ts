cd310af115ba4f78d87833727bd2a4d38089cbba
Jae Aeich <javedh@iitbhilai.ac.in>
2023-05-29 01:38:20 +0530

    feat: template generator prompt improvement

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,68 +1,128 @@
 import { PlopTypes } from '@turbo/gen';
 
+// Convert a string to kebab-case
+function kebabCase(str) {
+  return str
+    // Replace lowercase letter followed by an uppercase letter with a hyphen in between
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    // Replace whitespace and underscores with hyphens
+    .replace(/[\s_]+/g, '-')
+    // Convert the string to lowercase
+    .toLowerCase();
+}
+
+function validateNotEmpty(input:string) {
+  if (!input.length) {
+    return 'This field cannot be left empty!';
+  }
+  return true;
+}
+
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   plop.setGenerator('packages', {
     description: 'Generate a base config for new package',
     prompts: [
       {
+        type: 'list',
+        name: 'library',
+        message: 'Select a library the package will belong to:',
+        choices: ['ecc'],
+      },
+      {
+        type: 'list',
+        name: 'libraryDomain',
+        message: 'Select domain of the package:',
+        choices: ['client', 'dashboard', { name: 'Fill custom input for this field', value: '__custom__' }],
+      },
+      {
+        // If custom input selected for subdomain take the input
         type: 'input',
-        name: 'name',
-        message:
-        'What is the name of the package?',
-        validate: (input) => {
-          if (!input.match(/^[a-z0-9]+(-[a-z0-9]+)*$/)) {
-            return 'Name must be in kebab-case and in lower case letters. Example: my-package';
-          }
-          return true;
-        },
+        name: 'customLibraryDomain',
+        message: 'Enter custom library domain:',
+        when: (data) => data.libraryDomain === '__custom__',
+        validate: validateNotEmpty,
+      },
+      {
+        type: 'input',
+        name: 'librarySubDomain',
+        message: 'Enter library Subdomain (to skip press ENTER) :',
+      },
+      {
+        type: 'input',
+        name: 'organisation',
+        message: 'Enter organization name if applicable (to skip press ENTER) :',
+      },
+      {
+        type: 'input',
+        name: 'product',
+        message: 'Enter product name (cannot be left blank) :',
+        validate: validateNotEmpty,
       },
       {
         type: 'input',
         name: 'description',
         message: 'Write a description for the package',
+        validate: validateNotEmpty,
       },
     ],
-    actions: [
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/images/logo-elixir-cloud-aai.svg',
-        templateFile: 'templates/images-template/logo-elixir-cloud-aai.svg.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/images/logo-elixir.svg',
-        templateFile: 'templates/images-template/logo-elixir.svg.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/package.json',
-        templateFile: 'templates/package.json.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/package-lock.json',
-        templateFile: 'templates/package-lock.json.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/.eslintrc',
-        templateFile: 'templates/eslintrc.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/.eslintignore',
-        templateFile: 'templates//eslintignore.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/.gitignore',
-        templateFile: 'templates/gitignore.hbs',
-      },
-      {
-        type: 'add',
-        path: '{{ turbo.paths.root }}/packages/{{name}}/readme.md',
-        templateFile: 'templates/readme.md.hbs',
-      },
-    ],
+    actions: (data: any) => {
+      if (data) {
+        // Take the prompt value and convert it into kebab-case
+        const library = data.library || '';
+        const libraryDomain = data.libraryDomain === '__custom__' ? data.customLibraryDomain : data.libraryDomain;
+        const librarySubDomain = data.librarySubDomain || '';
+        const organisation = data.organisation || '';
+        const product = data.product || '';
+
+        const name = [library, libraryDomain, librarySubDomain, organisation, product].filter(Boolean).join('-');
+        const kebabName = kebabCase(name);
+
+        if (kebabName.length > 128) {
+          return [
+            {
+              type: 'run',
+              // Empty template to not generate any file
+              template: '',
+              // Error message
+              message: 'Package name is too long, please shorten it. (Package name cannot be greater than 128 characters)',
+            },
+          ];
+        }
+        // Add a new field "name" to inquirer.Answers
+        data.name = kebabName;
+        return [
+          {
+            type: 'addMany',
+            destination: '{{ turbo.paths.root }}/packages/{{ name }}',
+            templateFiles: 'templates/**/*.hbs',
+            stripExtensions: ['hbs'],
+            abortOnFail: true,
+          },
+          {
+            type: 'add',
+            path: '{{ turbo.paths.root }}/packages/{{ name }}/.eslintrc',
+            templateFile: 'templates/.eslintrc.hbs',
+          },
+          {
+            type: 'add',
+            path: '{{ turbo.paths.root }}/packages/{{ name }}/.eslintignore',
+            templateFile: 'templates/.eslintignore.hbs',
+          },
+          {
+            type: 'add',
+            path: '{{ turbo.paths.root }}/packages/{{ name }}/.gitignore',
+            templateFile: 'templates/.gitignore.hbs',
+          },
+        ];
+      }
+      return [
+        {
+          type: 'run',
+          template: '',
+          // Error message
+          message: 'Please try again, some internal error occured',
+        },
+      ];
+    },
   });
 }
