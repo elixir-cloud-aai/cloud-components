64e84a309ff8fab7d7466e903761d2e66941c7d2
Jae Aeich <javedh@iitbhilai.ac.in>
2023-06-07 16:43:13 +0530

    feat(component): this is package for components for TES
    fix #51

---
+++ packages/ecc-client-ga4gh-tes/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Import DesingSystem and envoke/register all the components here
+ */
+import { DesignSystem } from '@microsoft/fast-foundation';
+
+DesignSystem.getOrCreate().withPrefix('ecc').register();
