020c0ec5cb9486fefc33dc0b00197db82106c448
Jae Aeich <javedh@iitbhilai.ac.in>
2023-06-13 17:50:56 +0530

    refactor: use prebuild component instead of building custom

--- packages/ecc-client-ga4gh-tes/src/components/tes-get-run/definition/tesGetRun.styles.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-run/definition/tesGetRun.styles.ts
@@ -3,22 +3,21 @@ import { css } from "@microsoft/fast-element";
 const styles = css`
   .container {
     display: block;
-    border: 1px solid #e0e0e0;
     border-radius: 4px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    color: black;
   }
 
   .collapsed-container {
     display: flex;
+    border: none;
+    width: 100%;
+    height: 100%;
     justify-content: space-between;
     align-items: center;
-    padding: 10px;
-    background-color: #f5f5f5;
-    transition: background-color 0.3s ease;
   }
 
   .container:hover {
-    background-color: #e0e0e0;
   }
 
   .right {
@@ -49,18 +48,17 @@ const styles = css`
     justify-content: center;
     cursor: pointer;
     margin: 10px;
-    color: #888;
     transition: color 0.2s ease, transform 0.2s ease;
   }
 
   .delete-icon:hover,
   .reload-icon:hover {
-    color: #ff0000;
     transform: scale(1.1);
   }
 
   .expanded-container {
     padding: 1rem;
+    color: black;
   }
 
   .meta-data {
@@ -81,7 +79,6 @@ const styles = css`
 
   .executor {
     padding: 0.5rem;
-    background-color: #f9f9f9;
     margin-bottom: 1rem;
     border-radius: 4px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
@@ -101,7 +98,6 @@ const styles = css`
 
   .log-entry {
     padding: 0.5rem;
-    background-color: #f9f9f9;
     margin-bottom: 1rem;
     border-radius: 4px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
@@ -118,7 +114,6 @@ const styles = css`
   .user-id {
     margin-bottom: 0.5rem;
     font-size: 0.9rem;
-    color: #555;
   }
 `;
 
