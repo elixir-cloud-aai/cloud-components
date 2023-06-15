eb9ebe794869791e2de385536772013eb8a728b4
Jae Aeich <javedh@iitbhilai.ac.in>
2023-06-16 03:26:23 +0530

    feat: add skeleton during render

--- packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
@@ -1,19 +1,37 @@
 import {
   provideFASTDesignSystem,
+  fastSkeleton,
   fastAccordion,
+  fastAccordionItem,
 } from "@microsoft/fast-components";
 import { html, repeat, when } from "@microsoft/fast-element";
 import TESGetRuns from "./tesGetRuns.js";
 import TESGetRun from "../../tes-get-run/index.js";
 
-provideFASTDesignSystem().register(fastAccordion());
-
-const loading = html<string>`<div class="loader">Loading</div>`;
+provideFASTDesignSystem().register(
+  fastAccordion(),
+  fastAccordionItem(),
+  fastSkeleton()
+);
 
 const template = html<TESGetRuns>`
-  ${when((x) => x.isLoading, loading)}
   <div class="container">
     <fast-accordion>
+      ${when(
+        (x) => x.isLoading,
+        html` ${repeat(
+          (x) => Array.from(Array(parseInt(x.pageSize, 10))),
+          html`
+            <fast-accordion-item>
+              <fast-skeleton
+                style="border-radius: 4px;height: 100%;margin-bottom: 10px;"
+                shape="rect"
+                slot="heading"
+              ></fast-skeleton>
+            </fast-accordion-item>
+          `
+        )}`
+      )}
       ${repeat(
         (x) => x.data,
         html<TESGetRun>`
