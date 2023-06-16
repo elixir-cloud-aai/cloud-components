08b45646e83141131b847b0680c822b2caa486e7
Jae Aeich <javedh@iitbhilai.ac.in>
2023-06-17 03:44:32 +0530

    feat: add pagination to tes-get-runs

--- packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
@@ -3,6 +3,8 @@ import {
   fastSkeleton,
   fastAccordion,
   fastAccordionItem,
+  fastTab,
+  fastTabs,
 } from "@microsoft/fast-components";
 import { html, repeat, when } from "@microsoft/fast-element";
 import TESGetRuns from "./tesGetRuns.js";
@@ -11,39 +13,84 @@ import TESGetRun from "../../tes-get-run/index.js";
 provideFASTDesignSystem().register(
   fastAccordion(),
   fastAccordionItem(),
-  fastSkeleton()
+  fastSkeleton(),
+  fastTab(),
+  fastTabs()
 );
 
 const template = html<TESGetRuns>`
   <div class="container">
-    <fast-accordion>
+    <div class="search"></div>
+    <div class="list">
+      <fast-accordion>
+        ${when(
+          (x) => x.isLoading,
+          html` ${repeat(
+            (x) => Array.from(Array(parseInt(x.pageSize, 10))),
+            html`
+              <fast-accordion-item>
+                <fast-skeleton
+                  style="border-radius: 4px;height: 100%;margin-bottom: 10px;"
+                  shape="rect"
+                  slot="heading"
+                ></fast-skeleton>
+              </fast-accordion-item>
+            `
+          )}`
+        )}
+        ${repeat(
+          (x) => x.data,
+          html<TESGetRun>`
+            <ecc-tes-get-run
+              class="run-item"
+              id=${(x) => x.id}
+              state=${(x) => x.state}
+            >
+            </ecc-tes-get-run>
+          `
+        )}
+      </fast-accordion>
+    </div>
+    <div class="pagination">
       ${when(
         (x) => x.isLoading,
         html` ${repeat(
-          (x) => Array.from(Array(parseInt(x.pageSize, 10))),
+          () => Array.from(Array(4)),
           html`
-            <fast-accordion-item>
-              <fast-skeleton
-                style="border-radius: 4px;height: 100%;margin-bottom: 10px;"
-                shape="rect"
-                slot="heading"
-              ></fast-skeleton>
-            </fast-accordion-item>
+            <fast-skeleton
+              style="border-radius: 4px;height: 100%;margin-bottom: 10px;"
+              shape="rect"
+              slot="heading"
+            ></fast-skeleton>
           `
         )}`
       )}
       ${repeat(
-        (x) => x.data,
-        html<TESGetRun>`
-          <ecc-tes-get-run
-            class="run-item"
-            id=${(x) => x.id}
-            state=${(x) => x.state}
-          >
-          </ecc-tes-get-run>
-        `
+        (x) =>
+          Array.from(
+            { length: x.pageNumberOffset },
+            (_, index) => index + x.firstPageNumber
+          ),
+        html`<fast-button
+          appearance="outline"
+          style="color:black"
+          @click=${(x, c) => c.parent.handleClick(x)}
+          >${(x) => x}</fast-button
+        >`
       )}
-    </fast-accordion>
+      <fast-button appearance="neutral" @click=${(x) => x.handleNext()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            d="M5.055 7.06c-1.25-.714-2.805.189-2.805 1.628v8.123c0 1.44 1.555 2.342 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.342 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256L14.805 7.06C13.555 6.346 12 7.25 12 8.688v2.34L5.055 7.06z"
+          />
+        </svg>
+      </fast-button>
+    </div>
   </div>
 `;
 
--- packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.ts
@@ -18,24 +18,76 @@ interface Task {
   styles,
 })
 export default class TESGetRuns extends FASTElement {
+  // Number of Task to be listed at once
   @attr pageSize = 5;
 
-  @attr nextPageToken = "";
+  @observable nextPageToken: string | null = null;
 
+  // Data to be rendered
   @observable data: Task[] = [];
+
+  // Data cache for at max 3 times the pageSize
+  @observable cachedData: Task[] = [];
+
+  // First page number of every data cache
+  @observable firstPageNumber = 1;
+
+  // Number of pageSize data present in the cache
+  @observable pageNumberOffset = 0;
+
+  @observable pageNumberArray: number[] = [];
 
   @observable isLoading = true;
 
   async connectedCallback() {
     super.connectedCallback();
-    const newData = await fetchTasks();
-    if (newData && newData.tasks) {
-      this.data = newData.tasks;
-    }
-    this.isLoading = false;
+    // Since this is the first call, fetch the first page, no token needed
+    await this.fetchData("");
   }
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
-  }
+  /**
+   *Fetches data of 3*pageSize length and sets it as cache
+   * @param token token for the next page for cache data
+   */
+  fetchData = async (token: string) => {
+    this.isLoading = true;
+
+    // Reset data
+    this.data = [];
+
+    // Next first page number would start after offset of this data
+    if (token !== "") this.firstPageNumber += this.pageNumberOffset;
+
+    // Fetch new data
+    const newData = await fetchTasks(this.pageSize * 3, token);
+    if (newData && newData.tasks) {
+      this.cachedData = newData.tasks;
+      this.data = this.cachedData.slice(0, this.pageSize);
+      this.nextPageToken = newData.next_page_token;
+      this.pageNumberOffset = Math.ceil(this.cachedData.length / this.pageSize);
+      const array = [];
+      for (let i = 0; i < this.pageNumberOffset; i += 1) {
+        array.push(i + this.firstPageNumber);
+      }
+      this.pageNumberArray = array;
+    }
+    this.isLoading = false;
+  };
+
+  handleNext = async () => {
+    await this.fetchData(this.nextPageToken as string);
+  };
+
+  /**
+   *This method is fired when user, click to change pages within cache
+   * @param pageNumber Page number of pagination
+   */
+  handleClick = (pageNumber: number): void => {
+    const idx: number = pageNumber - this.firstPageNumber;
+
+    const startIndex: number = idx * this.pageSize;
+    const endIndex: number = (idx + 1) * this.pageSize;
+
+    this.data = this.cachedData.slice(startIndex, endIndex);
+  };
 }
