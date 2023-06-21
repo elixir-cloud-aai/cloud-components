611762e8735345287f14ede66f9a586c37209a94
Jae Aeich <javedh@iitbhilai.ac.in>
2023-06-21 14:04:55 +0530

    chore(ecc-tes-get-runs): remove previous pagination logic

--- packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.template.ts
@@ -74,7 +74,7 @@ const template = html<TESGetRuns>`
       ${when(
     x => x.isLoading,
     html` ${repeat(
-      () => Array.from(Array(4)),
+      () => Array.from(Array(3)),
       html`
             <fast-button appearance="outline">
               <fast-skeleton
@@ -89,18 +89,23 @@ const template = html<TESGetRuns>`
       ${when(
     x => !x.isLoading,
     html`
-          ${repeat(
-    x => Array.from(
-      { length: x.pageNumberOffset },
-      (_, index) => index + x.firstPageNumber,
-    ),
-    html`<fast-button
-              appearance="outline"
-              style="color:black"
-              @click=${(x, c) => c.parent.handleClick(x)}
-              >${x => x}</fast-button
-            >`,
-  )}
+          <fast-button
+            appearance="neutral"
+            ?disabled=${x => x.prevPageTokenStack.length === 0}
+            @click=${x => x.handlePrev()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z"
+              />
+            </svg>
+          </fast-button>
+          <fast-button appearance="outline" style="color:black">${x => x.pageNumber}</fast-button>
           <fast-button
             appearance="neutral"
             ?disabled=${x => x.nextPageToken === undefined}
--- packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-get-runs/defintion/tesGetRuns.ts
@@ -23,19 +23,13 @@ export default class TESGetRuns extends FASTElement {
 
   @observable nextPageToken = '';
 
+  @observable prevPageTokenStack = [];
+
   // Data to be rendered
   @observable data: Task[] = [];
 
-  // Data cache for at max 3 times the pageSize
-  @observable cachedData: Task[] = [];
-
   // First page number of every data cache
-  @observable firstPageNumber = 1;
-
-  // Number of pageSize data present in the cache
-  @observable pageNumberOffset = 0;
-
-  @observable pageNumberArray: number[] = [];
+  @observable pageNumber = 1;
 
   // Seach input for name_prefix filter
   @observable searchInput = '';
@@ -53,8 +47,9 @@ export default class TESGetRuns extends FASTElement {
   }
 
   /**
-   *Fetches data of 3*pageSize length and sets it as cache
+   *Fetches data of pageSize length
    * @param token token for the next page for cache data
+   * @param namePrefix takes in the filter value for name prefix
    */
   fetchData = async (token: string, namePrefix = '') => {
     this.isLoading = true;
@@ -62,65 +57,34 @@ export default class TESGetRuns extends FASTElement {
     // Reset data
     this.data = [];
 
-    // Next first page number would start after offset of this data
-    if (token !== '') this.firstPageNumber += this.pageNumberOffset;
-
     // Fetch new data
     let newData = [];
-    if (namePrefix === '') newData = await fetchTasks(this.pageSize * 3, token);
-    else {
-      newData = await fetchTasks(
-        this.pageSize * 3,
-        token,
-        'MINIMAL',
-        namePrefix,
-      );
-    }
+    newData = await fetchTasks(this.pageSize, token, 'MINIMAL', namePrefix);
     if (newData && newData.tasks) {
-      this.cachedData = newData.tasks;
-      this.data = this.cachedData.slice(0, this.pageSize);
+      this.data = newData.tasks;
       this.unfilterdData = this.data;
       this.nextPageToken = newData.next_page_token;
-      this.pageNumberOffset = Math.ceil(this.cachedData.length / this.pageSize);
-      const array = [];
-      for (let i = 0; i < this.pageNumberOffset; i += 1) {
-        array.push(i + this.firstPageNumber);
-      }
-      this.pageNumberArray = array;
     }
     this.isLoading = false;
   };
 
-  // Cache new data
+  // Cache next click
   handleNext = async () => {
+    this.stateInput = 'ALL';
+    this.pageNumber += 1;
     this.fetchData(this.nextPageToken as string, this.searchInput);
   };
 
-  /**
-   *This method is fired when user, click to change pages within cache
-   * @param pageNumber Page number of pagination
-   */
-  handleClick = (pageNumber: number): void => {
-    const idx: number = pageNumber - this.firstPageNumber;
-
-    const startIndex: number = idx * this.pageSize;
-    const endIndex: number = (idx + 1) * this.pageSize;
-
-    this.data = this.cachedData.slice(startIndex, endIndex);
-
-    // Set unfiltered data for state filterinng
-    this.unfilterdData = this.data;
-
-    // Reset state filter
-    this.stateInput = 'ALL';
-  };
+  // // Handle prev click
+  // handlePrev = async () => {
+  //   // TODO : Implement prev click
+  // };
 
   handleNameInput(event: Event) {
     this.searchInput = (event.target as HTMLInputElement).value;
 
     // reset the pages
-    this.firstPageNumber = 1;
-    this.pageNumberOffset = 0;
+    this.pageNumber = 1;
 
     // Fetch new data
     this.fetchData('', this.searchInput);
