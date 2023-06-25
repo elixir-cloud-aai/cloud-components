b51c5c6f641152b102a836f2d908e5166e1ff1ac
ltwmori <ltwmori@gmail.com>
2023-06-26 00:08:02 +0600

    feat: add pagination

--- packages/ecc-client-ga4gh-trs/src/components/index.ts
+++ packages/ecc-client-ga4gh-trs/src/components/index.ts
@@ -1,8 +1,6 @@
-import { CopyClipboard } from "./copy/copy.js";
-import { Search } from "@microsoft/fast-foundation";
-import { TRS } from "./trs/trs.js";
-import { Pagination } from "./pagination/pagination.js";
 
-const components = [CopyClipboard, Search, TRS, Pagination]
+import { TRS } from "./trs/trs.js";
+
+const components = [TRS]
 
 export default components;  
