d1d6ab226e50faa91beef4d470ca133433d7a92e
Javed Habib <jh4official@gmail.com>
2023-07-02 17:14:23 +0530

    style(ecc-client-ga4gh-tes): enforce better coding styles to make code more readable

--- packages/ecc-client-ga4gh-tes/src/components/tes-runs/test/tesRuns.unit.test.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-runs/test/tesRuns.unit.test.ts
@@ -7,21 +7,21 @@ DesignSystem.getOrCreate().register(TESRuns);
 describe('TESGetRuns', () => {
   it('should have a default baseURL of an empty string', async () => {
     const el = await fixture<TESRuns>(
-      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`,
+      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`
     );
     expect(el.baseURL).to.equal('');
   });
 
   it('should have a default pageSize of 5', async () => {
     const el = await fixture<TESRuns>(
-      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`,
+      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`
     );
     expect(el.pageSize).to.equal(5);
   });
 
   it('should initialize with isLoading set to true', async () => {
     const el = await fixture<TESRuns>(
-      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`,
+      html`<ecc-client-ga4gh-tes-runs></ecc-client-ga4gh-tes-runs>`
     );
     expect(el.isLoading).to.equal(true);
   });
