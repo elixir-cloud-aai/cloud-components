91f04f5fb4979bec504691821970bcecbf116441
salihuDickson <salihudickson@gmail.com>
2023-07-02 22:18:53 +0100

    feat(tes-create-run): setup executors fields duplication
    setup executors fields duplication to allow multiple inputs collection for executors fields

--- packages/ecc-client-ga4gh-tes/src/components/tes-create-run/definition/tesCreateRun.styles.ts
+++ packages/ecc-client-ga4gh-tes/src/components/tes-create-run/definition/tesCreateRun.styles.ts
@@ -20,6 +20,7 @@ const styles = css`
     width: 100%;
     display: flex;
     align-items: center;
+    margin-bottom: 7px;
   }
 
   .checkbox-field {
@@ -28,6 +29,44 @@ const styles = css`
 
   .submit-button:hover {
     background-color: #45a049;
+  }
+
+  .executor-container {
+    margin: 30px 0;
+    padding: 20px;
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+  }
+
+  .executor-container .delete {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .executor-container .heading {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .executors {
+    margin-bottom: 30px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .executors .command {
+    grid-column: 1/3;
+    width: 100%;
+  }
+
+  .executors label {
+    width: 70px;
+    margin-right: 20px;
+  }
+
+  .executors fast-text-field {
+    width: calc(100% - 120px);
   }
 
   .df {
