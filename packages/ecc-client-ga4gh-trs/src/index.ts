b827c6da63803125629044846ca464006f89843d
Assel Abzalova <ltwmori@gmail.com>
2023-07-31 12:45:49 +0600

    feat(trs): create TRS component (#55)
    Signed-off-by: Salihu <91833785+SalihuDickson@users.noreply.github.com>
    Signed-off-by: Assel Abzalova <ltwmori@gmail.com>
    Co-authored-by: Salihu <91833785+SalihuDickson@users.noreply.github.com>
    Co-authored-by: Javed Habib <jh4official@gmail.com>
    Co-authored-by: salihuDickson <salihudickson@gmail.com>

--- packages/ecc-client-ga4gh-trs/src/index.ts
+++ packages/ecc-client-ga4gh-trs/src/index.ts
@@ -1,1 +1,17 @@
-import "./design-system.js";
+/**
+ * Import DesingSystem and envoke/register all the components here
+ */
+import { DesignSystem } from "@microsoft/fast-foundation";
+import components from "./components/trs-list/index.js";
+import { componentConfig } from "./components/config.js";
+
+// Register all the components
+
+components.forEach((component) => {
+  DesignSystem.getOrCreate()
+    .withShadowRootMode("open")
+    .withPrefix(componentConfig.prefix)
+    .register({
+      ...component,
+    });
+});
