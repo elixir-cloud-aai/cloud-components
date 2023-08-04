6d78bb3033ca7ab9eb150c9dff59511852443957
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-08-04 22:39:17 +0530

    feat(ecc-client-ga4gh-tes): create run component (#81)
    Signed-off-by: Salihu <91833785+SalihuDickson@users.noreply.github.com>
    Signed-off-by: Javed Habib <100477031+JaeAeich@users.noreply.github.com>
    Co-authored-by: salihuDickson <salihudickson@gmail.com>
    Co-authored-by: Salihu <91833785+SalihuDickson@users.noreply.github.com>

--- packages/ecc-client-ga4gh-tes/src/components/index.ts
+++ packages/ecc-client-ga4gh-tes/src/components/index.ts
@@ -1,6 +1,7 @@
 import TESRun from './tes-run/index.js';
 import TESRuns from './tes-runs/index.js';
+import TESCreateRun from './tes-create-run/index.js';
 
-const components = [TESRuns, TESRun];
+const components = [TESRuns, TESRun, TESCreateRun];
 
 export default components;
