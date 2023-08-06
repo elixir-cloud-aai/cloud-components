12d699984567866ae5a4e7908f69db73536221fd
Javed Habib <jh4official@gmail.com>
2023-08-06 18:53:54 +0530

    feat: create form and add styles

--- packages/ecc-client-ga4gh-wes/src/components/wes-create-run/definition/wesCreateRun.template.ts
+++ packages/ecc-client-ga4gh-wes/src/components/wes-create-run/definition/wesCreateRun.template.ts
@@ -1,5 +1,83 @@
 import { html } from '@microsoft/fast-element';
+import WESCreateRun from './wesCreateRun.js';
 
-const template = html` <div>Create Run</div> `;
+const template = html<WESCreateRun>`
+  <form>
+    <div class="label-input">
+      <label for="workflow_params">workflow_params</label>
+      <input
+        type="text"
+        id="workflow_params"
+        name="workflow_params"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="workflow_type">workflow_type</label>
+      <input
+        type="text"
+        id="workflow_type"
+        name="workflow_type"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="workflow_type_version">workflow_type_version</label>
+      <input
+        type="text"
+        id="workflow_type_version"
+        name="workflow_type_version"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="tags">tags</label>
+      <input
+        type="text"
+        id="tags"
+        name="tags"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="workflow_engine_parameters">workflow_engine_parameters</label>
+      <input
+        type="text"
+        id="workflow_engine_parameters"
+        name="workflow_engine_parameters"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="workflow_url">workflow_url</label>
+      <input
+        type="text"
+        id="workflow_url"
+        name="workflow_url"
+        @input=${(x, c) => x.handleInput(c.event)}
+        required
+      />
+    </div>
+    <div class="label-input">
+      <label for="workflow_attachment">workflow_attachment</label>
+      <input
+        type="file"
+        id="workflow_attachment"
+        name="workflow_attachment"
+        @input=${(x, c) => x.handleInput(c.event)}
+        multiple
+        required
+      />
+    </div>
+    <div class="submit">
+      <button type="submit" @click=${(x) => x.handleSubmit()}>Submit</button>
+    </div>
+  </form>
+`;
 
 export default template;
--- packages/ecc-client-ga4gh-wes/src/components/wes-create-run/definition/wesCreateRun.ts
+++ packages/ecc-client-ga4gh-wes/src/components/wes-create-run/definition/wesCreateRun.ts
@@ -1,4 +1,9 @@
-import { FASTElement, attr, customElement } from '@microsoft/fast-element';
+import {
+  FASTElement,
+  attr,
+  customElement,
+  observable,
+} from '@microsoft/fast-element';
 import template from './wesCreateRun.template.js';
 import styles from './wesCreateRun.styles.js';
 
@@ -10,4 +15,32 @@ import styles from './wesCreateRun.styles.js';
 export default class WESCreateRun extends FASTElement {
   // Base URL, provided by app author
   @attr baseURL = '';
+
+  @observable data: FormData = new FormData();
+
+  handleInput = (event: Event) => {
+    const input = event.target as HTMLInputElement;
+    const { name } = input;
+
+    // If it's a file input, append each file to the FormData
+    if (input.type === 'file') {
+      for (const file of input.files as FileList) {
+        // Check if the key exists in the FormData, and if so, delete it before appending the updated value
+        if (this.data.has(name)) {
+          this.data.delete(name);
+        }
+        this.data.append(name, file);
+      }
+    } else {
+      // Check if the key exists in the FormData, and if so, delete it before appending the updated value
+      if (this.data.has(name)) {
+        this.data.delete(name);
+      }
+      this.data.append(name, input.value);
+    }
+  };
+
+  handleSubmit = () => {
+    console.log(...this.data);
+  };
 }
