9641c3f85b80bf33c219c280e8b78892086c5189
Anurag Gupta <me@anuragxd.com>
2023-08-12 05:06:12 +0530

    feat(ecc-utils-design): shoelace styling

--- packages/ecc-utils-design/src/components/button/button.template.ts
+++ packages/ecc-utils-design/src/components/button/button.template.ts
@@ -3,8 +3,5 @@ import { Button, ButtonOptions } from "@microsoft/fast-foundation";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
 
 export const template: ViewTemplate<Button, ButtonOptions> = html`
-  <button class="button">
-    <slot></slot>
-  </button>
-  <sl-button>Button</sl-button>
+  <sl-button><slot></slot></sl-button>
 `;
