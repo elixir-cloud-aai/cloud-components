0d708c408c332ab3a189acb8f9857089a4310b69
Anurag Gupta <me@anuragxd.com>
2023-08-12 04:51:15 +0530

    feat(ecc-utils-design): shoelace integration
    #103

--- packages/ecc-utils-design/src/components/button/button.styles.ts
+++ packages/ecc-utils-design/src/components/button/button.styles.ts
@@ -1,5 +1,6 @@
 import { css, ElementStyles } from "@microsoft/fast-element";
 import { backgroundColor, textColor } from "../../design-system/tokens.js";
+import "@shoelace-style/shoelace/dist/themes/light.css" assert { type: "css" };
 
 export const styles: ElementStyles = css`
   :host[hidden] {
--- packages/ecc-utils-design/src/components/button/button.template.ts
+++ packages/ecc-utils-design/src/components/button/button.template.ts
@@ -1,8 +1,10 @@
 import { ViewTemplate, html } from "@microsoft/fast-element";
 import { Button, ButtonOptions } from "@microsoft/fast-foundation";
+import "@shoelace-style/shoelace/dist/components/button/button.js";
 
 export const template: ViewTemplate<Button, ButtonOptions> = html`
   <button class="button">
     <slot></slot>
   </button>
+  <sl-button>Button</sl-button>
 `;
