3c66608f904721a2bd35f99450e979d3da05eaf5
salihuDickson <salihudickson@gmail.com>
2023-11-11 11:45:04 +0100

    chore(ecc-utils-design): code lint

--- packages/ecc-utils-design/scripts/build.js
+++ packages/ecc-utils-design/scripts/build.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { globby } from 'globby';
-import * as tsup from 'tsup';
-import { program } from 'commander';
+import { globby } from "globby";
+import * as tsup from "tsup";
+import { program } from "commander";
 
-program.option('-w --watch');
+program.option("-w --watch");
 program.parse();
 const options = program.opts();
 
@@ -13,14 +13,14 @@ const options = program.opts();
 // write script for react builds
 
 const config = {
-  format: 'esm',
-  target: 'es2017',
-  entry: [...(await globby('./src/components/**/!(*.(style|test)).ts'))],
+  format: "esm",
+  target: "es2017",
+  entry: [...(await globby("./src/components/**/!(*.(style|test)).ts"))],
   splitting: true,
   treeshake: true,
   bundle: true,
-  outDir: 'dist',
-  clean: 'true',
+  outDir: "dist",
+  clean: "true",
   dts: true,
   watch: options.watch,
 };
