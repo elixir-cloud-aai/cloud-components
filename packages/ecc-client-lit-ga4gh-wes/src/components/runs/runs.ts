1897b4900e154f4e0e07170780c3b1871b6bc191
Jae Aeich <jh4official@gmail.com>
2023-11-16 05:18:50 +0530

    feat: create wes runs

---
+++ packages/ecc-client-lit-ga4gh-wes/src/components/runs/runs.ts
@@ -0,0 +1,104 @@
+import { html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import "@elixir-cloud/design";
+import { fetchWorkflows } from "../../API/Workflow/wesGet.js";
+
+@customElement("ecc-client-lit-ga4gh-wes-runs")
+export class WESRuns extends LitElement {
+  @property({ type: Number }) private pageSize = 5;
+  @property({ type: String }) private baseURL =
+    "https://prowes.rahtiapp.fi/ga4gh/wes/v1";
+
+  @state() private filters: any[] = [
+    {
+      key: "title",
+      type: "search",
+      placeholder: "Search",
+    },
+    {
+      key: "tag",
+      type: "select",
+      options: [
+        "UNKNOWN",
+        "QUEUED",
+        "INITIALIZING",
+        "RUNNING",
+        "PAUSED",
+        "COMPLETE",
+        "EXECUTOR_ERROR",
+        "SYSTEM_ERROR",
+        "CANCELED",
+        "PREEMPTED",
+        "CANCELING",
+      ],
+      placeholder: "Filter by status",
+      selectConfig: {
+        multiple: true,
+      },
+    },
+  ];
+
+  @state() private items: any[] = [];
+  @state() private nextPageToken: string | null = "";
+
+  constructor() {
+    super();
+    const eccutilsDesignCollection = this.shadowRoot?.querySelector(
+      "ecc-utils-design-collection"
+    );
+    eccutilsDesignCollection?.setPage(this.pageSize);
+    this.fetchData();
+  }
+
+  private async fetchData() {
+    try {
+      // If all the items have been cached, don't invoke API call
+      if (this.nextPageToken === null) return;
+      const data = await fetchWorkflows(
+        this.baseURL,
+        this.pageSize,
+        this.nextPageToken
+      );
+
+      // Set nextPageToken to null if at the last page
+      if (data.next_page_token === "") this.nextPageToken = null;
+      else this.nextPageToken = data.next_page_token;
+
+      const covertedData: any[] = [];
+      data.runs.forEach((run: { run_id: string; state: string }) => {
+        covertedData.push({
+          index: this.items.length + covertedData.length + 1,
+          name: run.run_id,
+          key: run.run_id,
+          lazy: true,
+          tag: {
+            name: run.state,
+          },
+        });
+      });
+
+      this.items = [...this.items, ...covertedData];
+    } catch (error) {
+      console.error({
+        error,
+        breakPoint: "WESRuns.fetchData",
+      });
+    }
+  }
+
+  render() {
+    return html`
+      <ecc-utils-design-collection
+        .filters=${this.filters}
+        .items=${this.items}
+        @page-change=${this.fetchData}
+      >
+      </ecc-utils-design-collection>
+    `;
+  }
+}
+declare global {
+  interface HTMLElementTagNameMap {
+    "ecc-client-lit-ga4gh-wes-runs": WESRuns;
+  }
+}
