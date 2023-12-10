57aceb0a2cb4a73d769eb968bc353572b317fe5e
Jae Aeich <jh4official@gmail.com>
2023-12-10 09:42:18 +0530

    feat: change the way we deal with types and add logs

--- packages/ecc-client-lit-ga4gh-wes/src/components/runs/runs.ts
+++ packages/ecc-client-lit-ga4gh-wes/src/components/runs/runs.ts
@@ -1,6 +1,7 @@
 import { html, LitElement, render } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import "@elixir-cloud/design";
+import _ from "lodash-es";
 import { fetchWorkflow, fetchWorkflows } from "../../API/Workflow/wesGet.js";
 
 interface Children {
@@ -17,6 +18,22 @@ interface Children {
 interface Field {
   tabGroup: string;
   children: Array<Children>;
+}
+interface Child {
+  childName: string;
+  childPath: string;
+  type: "text" | "long-text" | "url" | "array" | "object";
+  arrayOptions?: {
+    vertical?: boolean;
+    pill?: boolean;
+  };
+}
+
+interface DataField {
+  tabGroupName: string;
+  // Group path is not needed
+  tabGroupPath: string;
+  children: Array<Child>;
 }
 
 interface ItemProp {
@@ -75,6 +92,96 @@ export class WESRuns extends LitElement {
     },
   ];
 
+  @state() private dataType: Array<DataField> = [
+    {
+      tabGroupName: "Request",
+      tabGroupPath: "request",
+      children: [
+        {
+          childName: "Tag",
+          childPath: "request.tags",
+          type: "text",
+        },
+        {
+          childName: "Workflow Engine Parameters",
+          type: "text",
+          childPath: "request.workflow_engine_parameters",
+        },
+        {
+          childName: "Workflow Params",
+          type: "text",
+          childPath: "request.workflow_params",
+        },
+        {
+          childName: "Workflow Type",
+          type: "text",
+          childPath: "request.workflow_type",
+        },
+        {
+          childName: "Workflow Type Version",
+          type: "text",
+          childPath: "request.workflow_type_version",
+        },
+        {
+          childName: "Workflow URL",
+          type: "url",
+          childPath: "request.workflow_url",
+        },
+      ],
+    },
+    {
+      tabGroupName: "Run Log",
+      tabGroupPath: "run_log",
+      children: [
+        {
+          childName: "Command",
+          type: "text",
+          childPath: "run_log.command",
+        },
+        {
+          childName: "Expires",
+          type: "text",
+          childPath: "run_log.expires",
+        },
+        {
+          childName: "Max Retries",
+          type: "text",
+          childPath: "run_log.max_retries",
+        },
+        {
+          childName: "Task Received",
+          type: "text",
+          childPath: "run_log.task_received",
+        },
+        {
+          childName: "Task Started",
+          type: "text",
+          childPath: "run_log.task_started",
+        },
+        {
+          childName: "Time Queue",
+          type: "text",
+          childPath: "run_log.time_queue",
+        },
+        {
+          childName: "UTC Offset",
+          type: "text",
+          childPath: "run_log.utc_offset",
+        },
+        {
+          childName: "Std Out",
+          type: "long-text",
+          childPath: "run_log.stdout",
+        },
+        {
+          childName: "Std Err",
+          type: "long-text",
+          childPath: "run_log.stderr",
+        },
+      ],
+    },
+  ];
+
   @state() private items: ItemProp[] = [];
   @state() private nextPageToken: string | null = "";
 
@@ -125,7 +232,6 @@ export class WESRuns extends LitElement {
 
         eccUtilsDesignCollection.totalItems = this.items.length;
       } else this.nextPageToken = data.next_page_token;
-      console.log(data);
       const covertedData: ItemProp[] = [];
       data.runs.forEach((run: { run_id: string; state: string }) => {
         covertedData.push({
@@ -154,40 +260,104 @@ export class WESRuns extends LitElement {
     }
   }
 
-  private _createFields(data: any): Array<Field> {
+  private _transformData(
+    dataType: Array<DataField>,
+    runData: any
+  ): Array<Field> {
     this.requestUpdate();
-    const groups = ["request", "runs logs", "task logs"];
+    const transformedData: Array<Field> = [];
 
-    const fields: Array<Field> = [];
+    // TODO: When proWES implements runLogs and taskLogs, remove this and
+    // define a better default dataType in the state itself
+    const typesHere = dataType;
 
-    for (const group of groups) {
-      const groupFields = [];
+    console.log(runData);
 
-      const groupData = data[group.replace(/\s/g, "_")];
+    for (let i = 0; i < runData.task_logs.length; i += 1) {
+      const taskType: DataField = {
+        tabGroupName: `Task Log ${i + 1}`,
+        tabGroupPath: `task_logs[${i + 1}]`,
+        children: [
+          {
+            childName: "Name",
+            type: "text",
+            childPath: `task_logs[${i + 1}].name`,
+          },
+          {
+            childName: "Task Id",
+            type: "text",
+            childPath: `task_logs[${i + 1}].id`,
+          },
+          {
+            childName: "Description",
+            type: "text",
+            childPath: `task_logs[${i + 1}].description`,
+          },
+          {
+            childName: "Creation Time",
+            type: "text",
+            childPath: `task_logs[${i + 1}].creation_time`,
+          },
+          {
+            childName: "Resources",
+            type: "object",
+            childPath: `task_logs[${i + 1}].resources`,
+          },
+          {
+            childName: "State",
+            type: "text",
+            childPath: `task_logs[${i + 1}].state`,
+          },
+          {
+            childName: "Tags",
+            type: "object",
+            childPath: `task_logs[${i + 1}].tags`,
+          },
+        ],
+      };
 
-      if (groupData && typeof groupData === "object") {
-        for (const [key, value] of Object.entries(groupData)) {
-          const child = {
-            key,
-            label:
-              key.charAt(0).toUpperCase() + key.slice(1).replace(/_/g, " "), // Convert underscores to spaces and capitalize
-            value,
-            type: typeof value === "object" ? "object" : "text", // Assume object type for nested objects
-          };
-
-          groupFields.push(child);
-        }
-
-        const field = {
-          tabGroup: group,
-          children: groupFields,
-        };
-
-        fields.push(field as Field);
-      }
+      typesHere.push(taskType);
     }
+    console.log(typesHere);
+    typesHere.forEach((tabGroup) => {
+      const { tabGroupName } = tabGroup;
+      const { tabGroupPath } = tabGroup;
+      const field: Field = {
+        tabGroup: tabGroupName,
+        children: [],
+      };
 
-    return fields;
+      const group = _.get(runData, tabGroupPath);
+
+      if (
+        group !== undefined ||
+        (Array.isArray(group) && group.length > 0) ||
+        (typeof group === "object" && Object.entries(group).length > 0)
+      ) {
+        tabGroup.children.forEach((child) => {
+          const childName =
+            child.childName || child.childPath.split(".").pop() || "undefined";
+          const { childPath } = child;
+          const { type } = child;
+
+          const value = _.get(runData, childPath);
+          if (value !== undefined) {
+            const children: Children = {
+              key: childPath,
+              label: childName,
+              value,
+              type,
+              // arrayOptions: child.arrayOptions,
+            };
+            field.children.push(children);
+          }
+        });
+
+        transformedData.push(field);
+      }
+    });
+
+    return transformedData;
   }
 
   private async handleExpandItem(e: CustomEvent) {
@@ -205,21 +375,18 @@ export class WESRuns extends LitElement {
     const runData = await fetchWorkflow(this.baseURL, detail.key);
     if (children.length === 0) {
       try {
-        const data = this._createFields(runData);
-        console.log(data);
-        // Create a child div and set its slot attribute
+        const nice = this._transformData(this.dataType, runData);
         const child = document.createElement("div");
         child.setAttribute("slot", key);
 
         // Use LitElement's html template to create the details component
         const detailsComponent = html`<ecc-utils-design-details
-          .fields=${data}
+          .fields=${nice}
         ></ecc-utils-design-details>`;
 
         // Render the details component using Lit's render function
-        render(detailsComponent, child);
-
         // Append the child to the target
+        render(detailsComponent, child);
         target.appendChild(child);
       } catch (error) {
         eccUtilsDesignCollection.error(`Failed to fetch data for run: ${key}`);
