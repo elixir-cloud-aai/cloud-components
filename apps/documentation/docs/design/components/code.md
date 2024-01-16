98016eaf0ad9b94919ce1e9f1125fce9ec8e654c
Jae Aeich <jh4official@gmail.com>
2024-01-16 16:34:34 +0530

    feat: add parts and docs

--- apps/documentation/docs/design/components/code.md
+++ apps/documentation/docs/design/components/code.md
@@ -36,14 +36,72 @@ import "@elixir-cloud/design/dist/components/code/index.js";
 | `language` | `false`  |         | `String`  | Specifies the language interpreter for syntax highlighting.            |
 | `lnu`      | `true`   |         | `Boolean` | Specifies whether the editor should display line numbers.              |
 
+## Methods
+
+| Method Name | Arguments | Description        |
+| ----------- | --------- | ------------------ |
+| `getCode()` |           | Returns the input. |
+
 ## Slots
 
 ## Parts
 
-| Part Name | Description |
-| --------- | ----------- |
+| Part Name                         | Description                                                                   |
+| --------------------------------- | ----------------------------------------------------------------------------- |
+| `container`                       | Wrapper container for code and header (select, copy, etc.)                    |
+| `header`                          | Header container                                                              |
+| [`code`](#code)                   | Code wrapper of this element (see the [css variable](#code) for more info).   |
+| `select-form-control`             | The form control that wraps the label, input, and help text.                  |
+| `select-form-control-label`       | The labels wrapper.                                                           |
+| `select-form-control-input`       | The selects wrapper.                                                          |
+| `select-form-control-help-text`   | The help texts wrapper.                                                       |
+| `select-combobox`                 | The container that wraps the prefix, combobox, clear icon, and expand button. |
+| `select-prefix`                   | The container that wraps the prefix slot.                                     |
+| `select-display-input`            | The element that displays the selected options label, an <input> element.     |
+| `select-listbox`                  | The listbox container where options are slotted.                              |
+| `select-tags`                     | The container that houses option tags when multiselect is used.               |
+| `select-tag`                      | The individual tags that represent each multiselect option.                   |
+| `select-tag__base`                | The tags base part.                                                           |
+| `select-tag__content`             | The tags content part.                                                        |
+| `select-tag__remove-button`       | The tags remove button.                                                       |
+| `select-tag__remove-button__base` | The tags remove button base part.                                             |
+| `select-clear-button`             | The clear button.                                                             |
+| `select-expand-icon`              | The container that wraps the expand icon.                                     |
+| `copy-button`                     | The internal `<button>` element.                                              |
+| `copy-icon`                       | The container that holds the copy icon.                                       |
+| `copy-success-icon`               | The container that holds the success icon.                                    |
+| `copy-error-icon`                 | The container that holds the error icon.                                      |
+| `copy-tooltip__base`              | The tooltips exported base part.                                              |
+| `copy-tooltip__base__popup`       | The tooltips exported popup part.                                             |
+| `copy-tooltip__base__arrow`       | The tooltips exported arrow part.                                             |
+| `copy-tooltip__body`              | The tooltips exported body part.                                              |
 
 ## CSS Variables
+
+### Code
+
+| Variable                 | Description                                        |
+| ------------------------ | -------------------------------------------------- |
+| `--font-family`          | The font family used for the code.                 |
+| `--font-size`            | The font size of the code.                         |
+| `--line-height`          | The line height of the code.                       |
+| `--lines-width`          | The width of the lines in the code.                |
+| `--editor-bg-color`      | The background color of the code editor.           |
+| `--editor-text-color`    | The text color of the code editor.                 |
+| `--editor-caret-color`   | The color of the caret in the code editor.         |
+| `--editor-sel-color`     | The color of the selected text in the code editor. |
+| `--lines-bg-color`       | The background color of the lines in the code.     |
+| `--lines-text-color`     | The text color of the lines in the code.           |
+| `--scroll-track-color`   | The color of the scroll track in the code editor.  |
+| `--scroll-thumb-color`   | The color of the scroll thumb in the code editor.  |
+| `--hl-color-string`      | The color for strings in code highlighting.        |
+| `--hl-color-function`    | The color for functions in code highlighting.      |
+| `--hl-color-number`      | The color for numbers in code highlighting.        |
+| `--hl-color-operator`    | The color for operators in code highlighting.      |
+| `--hl-color-class-name`  | The color for class names in code highlighting.    |
+| `--hl-color-punctuation` | The color for punctuation in code highlighting.    |
+| `--hl-color-keyword`     | The color for keywords in code highlighting.       |
+| `--hl-color-comment`     | The color for comments in code highlighting.       |
 
 ## Examples
 
--- packages/ecc-utils-design/src/components/code/code.ts
+++ packages/ecc-utils-design/src/components/code/code.ts
@@ -25,7 +25,23 @@ export default class EccUtilsDesignCode extends LitElement {
       }
 
       .error {
-        --editor-text-color: red;
+        --editor-text-color: var(--sl-color-danger-500);
+        --editor-caret-color: var(--sl-color-danger-500);
+        --editor-sel-color: var(--sl-color-danger-500);
+
+        --lines-text-color: var(--sl-color-danger-500);
+        --scroll-track-color: var(--sl-color-danger-500);
+        --scroll-thumb-color: var(--sl-color-danger-500);
+
+        /*lit-theme colors for default highlight tokens */
+        --hl-color-string: var(--sl-color-danger-500);
+        --hl-color-function: var(--sl-color-danger-500);
+        --hl-color-number: var(--sl-color-danger-500);
+        --hl-color-operator: var(--sl-color-danger-500);
+        --hl-color-class-name: var(--sl-color-danger-500);
+        --hl-color-punctuation: var(--sl-color-danger-500);
+        --hl-color-keyword: var(--sl-color-danger-500);
+        --hl-color-comment: var(--sl-color-danger-500);
       }
 
       .header {
@@ -45,6 +61,41 @@ export default class EccUtilsDesignCode extends LitElement {
   @state() languages: Language[] = ["YAML", "JSON", "Text"];
   @state() value = "";
   @state() error = false;
+
+  private cssParts = {
+    // Parts
+    container: "container", // container
+    header: "header", // header
+
+    code: "code", // code
+
+    // https://shoelace.style/components/select/#parts
+    selectFormControl: "form-control", // select-form-control
+    selectFormControlLabel: "form-control-label", // select-form-control-label
+    selectFormControlInput: "form-control-input", // select-form-control-input
+    selectFormControlHelpText: "form-control-help-text", // select- form-control-help-text
+    selectCombobox: "combobox", // select-combobox
+    selectPrefix: "prefix", // select-prefix
+    selectDisplayInput: "display-input", // select-display-input
+    selectListbox: "listbox", // select-listbox
+    selectTags: "tags", // select-tags
+    selectTag: "tag", // select-tag
+    selectTagBase: "tag__base", // tag__base
+    selectTagContent: "tag__content", // tag__content
+    selectTagRemoveButton: "tag__remove-button", // tag__remove-button
+    selectTagRemoveButtonBase: "tag__remove-button__base", // tag__remove-button__base
+    selectClearButton: "clear-button", // select-clear-button
+
+    // https://shoelace.style/components/copy-button/#parts
+    copyButtonButton: "copy-button-button", // copy-button
+    copyButtonCopyIcon: "copy-button-copy-icon", // copy-icon
+    copyButtonSuccessIcon: "copy-button-success-icon", // copy-success-icon
+    copyButtonErrorIcon: "copy-button-error-icon", // copy-error-icon
+    copyButtonTooltipBase: "copy-button-tooltip__base", // copy-tooltip__base
+    copyButtonTooltipBasePopup: "copy-button-tooltip__base__popup", // copy-tooltip__base__popup
+    copyButtonTooltipBaseArrow: "copy-button-tooltip__base__arrow", // copy-tooltip__base__arrow
+    copyButtonTooltipBody: "copy-button-tooltip__body", // copy-tooltip__body
+  };
 
   private _setCode() {
     const code = (this.shadowRoot?.getElementById("code") as any).getCode();
@@ -71,19 +122,66 @@ export default class EccUtilsDesignCode extends LitElement {
     }
   }
 
-  public returnCode() {
+  private _setLanguage(event: Event) {
+    this.language = (event.target as any).value;
+    this._setCode();
+  }
+
+  public getCode() {
     return this.value;
   }
 
   render() {
+    const {
+      container,
+      header,
+      code,
+      selectFormControl,
+      selectFormControlLabel,
+      selectFormControlInput,
+      selectFormControlHelpText,
+      selectCombobox,
+      selectPrefix,
+      selectDisplayInput,
+      selectListbox,
+      selectTags,
+      selectTag,
+      selectTagBase,
+      selectTagContent,
+      selectTagRemoveButton,
+      selectTagRemoveButtonBase,
+      selectClearButton,
+      copyButtonButton,
+      copyButtonCopyIcon,
+      copyButtonSuccessIcon,
+      copyButtonErrorIcon,
+      copyButtonTooltipBase,
+      copyButtonTooltipBasePopup,
+      copyButtonTooltipBaseArrow,
+      copyButtonTooltipBody,
+    } = this.cssParts;
     return html`
-      <div class="container">
-        <div class="header">
+      <div part=${container} class=${container}>
+        <div part=${header} class=${header}>
           <sl-select
+            exportparts="
+        					select-form-control: ${selectFormControl},
+        					select-form-control-label: ${selectFormControlLabel},
+        					select-form-control-input: ${selectFormControlInput},
+        					select-form-control-help-text: ${selectFormControlHelpText},
+        					select-combobox: ${selectCombobox},
+        					select-prefix: ${selectPrefix},
+        					select-display-input: ${selectDisplayInput},
+        					select-listbox: ${selectListbox},
+        					select-tags: ${selectTags},
+        					select-tag: ${selectTag},
+        					select-tag-base: ${selectTagBase},
+        					select-tag-content: ${selectTagContent},
+        					select-tag-remove-button: ${selectTagRemoveButton},
+        					select-tag-remove-button-base: ${selectTagRemoveButtonBase},
+        					select-clear-button: ${selectClearButton}"
             value=${this.language}
-            @sl-change=${(e: Event) =>
-              (this.language = (e.target as HTMLSelectElement)
-                .value as Language)}
+            @sl-change=${(e: Event) => this._setLanguage(e)}
           >
             ${this.languages.map(
               (language) =>
@@ -91,12 +189,22 @@ export default class EccUtilsDesignCode extends LitElement {
             )}
           </sl-select>
           <sl-copy-button
+            exportparts="
+        					copy-button: ${copyButtonButton},
+        					copy-icon: ${copyButtonCopyIcon},
+        					copy-success-icon: ${copyButtonSuccessIcon},
+        					copy-error-icon: ${copyButtonErrorIcon},
+        					tooltip__base: ${copyButtonTooltipBase},
+        					copy-tooltip__base__popup: ${copyButtonTooltipBasePopup},
+        					copy-tooltip__base__arrow: ${copyButtonTooltipBaseArrow},
+        					copy-tooltip__body: ${copyButtonTooltipBody}"
             value=${this.value}
             error-label="Nothing to copy!"
           ></sl-copy-button>
         </div>
         <lit-code
-          id="code"
+          id=${code}
+          part=${code}
           class=${this.error ? "error" : ""}
           ?linenumbers=${this.lnu}
           noshadow
