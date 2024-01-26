4e702354a5432a1070a7921d50fb8a74226bc164
salihuDickson <salihudickson@gmail.com>
2024-01-26 20:40:13 +0100

    fix(ecc-client-lit-ga4gh-wes): minor fixes

--- packages/ecc-client-lit-ga4gh-wes/src/components/create-run/index.ts
+++ packages/ecc-client-lit-ga4gh-wes/src/components/create-run/index.ts
@@ -1,12 +1,12 @@
-import WESCreateRuns from "./create-run.js";
+import WESCreateRun from "./create-run.js";
 
 export * from "./create-run.js";
-export default WESCreateRuns;
+export default WESCreateRun;
 
-window.customElements.define("ecc-client-ga4gh-wes-create-runs", WESCreateRuns);
+window.customElements.define("ecc-client-ga4gh-wes-create-run", WESCreateRun);
 
 declare global {
   interface HTMLElementTagNameMap {
-    "ecc-client-ga4gh-wes-create-runs": WESCreateRuns;
+    "ecc-client-ga4gh-wes-create-run": WESCreateRun;
   }
 }
