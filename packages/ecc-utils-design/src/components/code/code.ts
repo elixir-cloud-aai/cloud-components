11d26be9cb8e74c207a8de6f01f4aa03cd315416
Jae Aeich <jh4official@gmail.com>
2024-01-31 22:12:08 +0530

    chore: migrate code, remove dependency

--- packages/ecc-utils-design/src/components/code/code.ts
+++ packages/ecc-utils-design/src/components/code/code.ts
@@ -1,15 +1,11 @@
 import { LitElement, html, css } from "lit";
 import { property, state } from "lit/decorators.js";
-import jsyaml from "js-yaml";
 import "@shoelace-style/shoelace/dist/components/input/input.js";
 import "@shoelace-style/shoelace/dist/components/badge/badge.js";
-import "@shoelace-style/shoelace/dist/components/alert/alert.js";
-import "@shoelace-style/shoelace/dist/components/option/option.js";
+import "@shoelace-style/shoelace/dist/components/textarea/textarea.js";
 import "@shoelace-style/shoelace/dist/components/copy-button/copy-button.js";
 import { hostStyles } from "../../styles/host.styles.js";
 import getShoelaceStyles from "../../styles/shoelace.styles.js";
-import "prismjs";
-import "lit-code";
 
 type Language = "YAML" | "JSON" | "Text";
 
@@ -20,149 +16,279 @@ export default class EccUtilsDesignCode extends LitElement {
     ),
     hostStyles,
     css`
-      .container {
-        display: grid;
-      }
-
-      .error {
-        --editor-text-color: var(--sl-color-danger-500);
-        --editor-caret-color: var(--sl-color-danger-500);
-        --editor-sel-color: var(--sl-color-danger-500);
-
-        --lines-text-color: var(--sl-color-danger-500);
-        --scroll-track-color: var(--sl-color-danger-500);
-        --scroll-thumb-color: var(--sl-color-danger-500);
-
-        /*lit-theme colors for default highlight tokens */
-        --hl-color-string: var(--sl-color-danger-500);
-        --hl-color-function: var(--sl-color-danger-500);
-        --hl-color-number: var(--sl-color-danger-500);
-        --hl-color-operator: var(--sl-color-danger-500);
-        --hl-color-class-name: var(--sl-color-danger-500);
-        --hl-color-punctuation: var(--sl-color-danger-500);
-        --hl-color-keyword: var(--sl-color-danger-500);
-        --hl-color-comment: var(--sl-color-danger-500);
-      }
-
-      .header {
-        color: var(--sl-color-gray-500);
-        display: grid;
-        grid-template-columns: 1fr auto;
+      #label {
+        display: flex;
+        justify-content: space-between;
         align-items: center;
-        border-bottom: 1px solid var(--sl-panel-border-color);
+        gap: var(--sl-spacing-x-small);
       }
     `,
   ];
 
-  @state() code = "";
+  @property({ type: String }) code = "";
+  @property({ type: String }) label = "Code";
   @property({ type: String }) language: Language = "YAML";
   @property({ type: Boolean }) lnu = true;
+  @property({ type: Number }) indentation = 2;
 
   @state() value = "";
+  @state() elTextarea: HTMLTextAreaElement | null = null;
   @state() error = false;
+  @state() indent = "";
+  @state() currentIndentation = "";
+  @state() opening = ["(", "{", "[", "'", '"'];
+  @state() closing = [")", "}", "]", "'", '"'];
 
-  private cssParts = {
-    container: "container",
-    header: "header",
-    code: "code",
-    badgeBase: "base",
-    copyButtonButton: "copy-button-button",
-    copyButtonCopyIcon: "copy-button-copy-icon",
-    copyButtonSuccessIcon: "copy-button-success-icon",
-    copyButtonErrorIcon: "copy-button-error-icon",
-    copyButtonTooltipBase: "copy-button-tooltip__base",
-    copyButtonTooltipBasePopup: "copy-button-tooltip__base__popup",
-    copyButtonTooltipBaseArrow: "copy-button-tooltip__base__arrow",
-    copyButtonTooltipBody: "copy-button-tooltip__body",
-  };
+  private cssParts = {};
 
-  private _setCode() {
-    const code = (this.shadowRoot?.getElementById("code") as any).getCode();
-    this.value = code;
+  constructor() {
+    super();
+    this.indent = " ".repeat(this.indentation);
+  }
 
-    // validation
-    if (this.language === "JSON") {
-      try {
-        JSON.parse(this.value);
-        this.error = false;
-      } catch (error) {
-        this.error = true;
-      }
-    } else if (this.language === "YAML") {
-      try {
-        jsyaml.loadAll(this.value);
-        this.error = false;
-      } catch (error) {
-        this.error = true;
-      }
-    } else this.error = false;
+  private _getTextAreaEle(): HTMLTextAreaElement {
+    const slTextarea = this.shadowRoot?.querySelector("sl-textarea");
+    const ele = slTextarea?.shadowRoot?.querySelector("textarea");
+    if (!ele || !(ele instanceof HTMLTextAreaElement)) {
+      throw new Error("Could not find textarea element");
+    }
+    return ele;
+  }
 
-    this.dispatchEvent(
-      new CustomEvent("ecc-utils-change", {
-        detail: {
-          value: this.value,
-          language: this.language,
-          error: this.error,
-        },
-      })
-    );
+  firstUpdated() {
+    this._updateTextarea();
   }
 
   public getCode() {
-    const code = (this.shadowRoot?.getElementById("code") as any).getCode();
-    return code;
+    return this.code;
+  }
+
+  private _setCode(code: string) {
+    this.code = code;
+    this._updateTextarea();
+  }
+
+  private _setCursor(pos: number) {
+    this.elTextarea = this._getTextAreaEle();
+    this.elTextarea.setSelectionRange(pos, pos);
+  }
+
+  private _setSelect(from: number, to: number) {
+    this.elTextarea = this._getTextAreaEle();
+    this.elTextarea.setSelectionRange(from, to);
+  }
+
+  private _getCurrentLineIndent() {
+    const slTextarea = this.shadowRoot?.querySelector("sl-textarea");
+    const ele = slTextarea?.shadowRoot?.querySelector("textarea");
+    if (!ele || !(ele instanceof HTMLTextAreaElement)) {
+      throw new Error("Could not find textarea element");
+    }
+    this.elTextarea = ele;
+
+    const selStart = this.elTextarea.selectionStart;
+    const selEnd = this.elTextarea.selectionEnd;
+
+    const indentStart = this.code.lastIndexOf("\n", selStart - 1) + 1;
+    const spaces = (() => {
+      let pos = indentStart;
+      while (this.code[pos] === " " && pos < selEnd) pos += 1;
+      return pos - indentStart;
+    })();
+    return " ".repeat(spaces);
+  }
+
+  private _updateTextarea() {
+    if (!this.elTextarea) return;
+    this.elTextarea.value = this.code;
+
+    this.dispatchEvent(
+      new CustomEvent("ecc-utils-change", { detail: this.code })
+    );
+  }
+
+  private _insertCode(pos: number, text: string, placeCursor = true) {
+    this.code = this.code.substring(0, pos) + text + this.code.substring(pos);
+    this._updateTextarea();
+    if (placeCursor) this._setCursor(pos + text.length);
+  }
+
+  private _replaceCode(
+    posFrom: number,
+    posTo: number,
+    text = "",
+    placeCursor = true
+  ) {
+    this.code =
+      this.code.substring(0, posFrom) + text + this.code.substring(posTo);
+    this._updateTextarea();
+    if (placeCursor) this._setCursor(posFrom + text.length);
+  }
+
+  private _handleInput(e: InputEvent) {
+    this.code = (e.target as HTMLInputElement).value;
+    this.dispatchEvent(
+      new CustomEvent("ecc-utils-change", { detail: this.code })
+    );
+  }
+
+  private _handleTabs(e: KeyboardEvent) {
+    e.preventDefault();
+    const slTextarea = this.shadowRoot?.querySelector("sl-textarea");
+    const ele = slTextarea?.shadowRoot?.querySelector("textarea");
+    if (!ele || !(ele instanceof HTMLTextAreaElement)) {
+      throw new Error("Could not find textarea element");
+    }
+    this.elTextarea = ele;
+    const selStart = this.elTextarea.selectionStart;
+    const selEnd = this.elTextarea.selectionEnd;
+
+    if (selStart !== selEnd) {
+      // multiline indent
+      const selLineStart = Math.max(
+        0,
+        this.code.lastIndexOf("\n", selStart - 1)
+      );
+      const selLineEnd = Math.max(this.code.indexOf("\n", selEnd), selEnd);
+
+      let linesInChunk = 0;
+      let codeChunk = this.code.substring(selLineStart, selLineEnd);
+      let lenShift = this.indent.length;
+      if (selLineStart === 0) codeChunk = `\n${codeChunk}`;
+
+      if (e.shiftKey) {
+        // Unindent
+        lenShift = -lenShift;
+        linesInChunk = (
+          codeChunk.match(new RegExp(`\n${this.indent}`, "g")) || []
+        ).length;
+        codeChunk = codeChunk.replaceAll(`\n${this.indent}`, "\n");
+      } else {
+        // Indent
+        linesInChunk = (codeChunk.match(/\n/g) || []).length;
+        codeChunk = codeChunk.replaceAll("\n", `\n${this.indent}`);
+      }
+
+      if (selLineStart === 0) codeChunk = codeChunk.replace(/^\n/, "");
+      this._replaceCode(selLineStart, selLineEnd, codeChunk, false);
+
+      const newStart = Math.max(selLineStart + 1, selStart + lenShift);
+      const newEnd = selEnd + linesInChunk * lenShift;
+      this._setSelect(newStart, newEnd);
+    } else {
+      this._insertCode(selStart, this.indent, true);
+    }
+  }
+
+  private _handleBackspace(e: KeyboardEvent) {
+    this.elTextarea = this._getTextAreaEle();
+    const selStart = this.elTextarea.selectionStart;
+    const selEnd = this.elTextarea.selectionEnd;
+    if (e.ctrlKey || selStart !== selEnd) return;
+
+    e.preventDefault();
+
+    // remove brackets pairs
+    const prevSymbol = this.code[selStart - 1];
+    const curSymbol = this.code[selStart];
+    const isInPairs =
+      this.opening.includes(prevSymbol) && this.closing.includes(curSymbol);
+    const isPair = this.closing[this.opening.indexOf(prevSymbol)] === curSymbol;
+
+    if (isInPairs && isPair) {
+      this._replaceCode(selStart - 1, selStart + 1);
+    } else {
+      // remove indent
+      const chunkStart = selStart - this.indent.length;
+      const chunkEnd = selStart;
+      const chunk = this.code.substring(chunkStart, chunkEnd);
+
+      if (chunk === this.indent) this._replaceCode(chunkStart, chunkEnd);
+      else this._replaceCode(selStart - 1, selStart);
+    }
+  }
+
+  private _handleAutoClose(e: KeyboardEvent) {
+    this.elTextarea = this._getTextAreaEle();
+    const selStart = this.elTextarea.selectionStart;
+    const selEnd = this.elTextarea.selectionEnd;
+    if (this.code[selStart] === "'" || this.code[selStart] === '"') {
+      this._handleAutoSkip(e);
+      return;
+    }
+    e.preventDefault();
+
+    if (selStart === selEnd) {
+      const opening = e.key;
+      const closing = this.closing[this.opening.indexOf(opening)];
+
+      if (
+        opening === "{" &&
+        (this.code[selStart] === "\n" || this.code.length === selStart)
+      ) {
+        const lineShift = `\n${this._getCurrentLineIndent()}`;
+        this._insertCode(
+          selStart,
+          opening + lineShift + this.indent + lineShift + closing
+        );
+        this._setCursor(selStart + lineShift.length + this.indent.length + 1);
+      } else {
+        this._insertCode(selStart, opening + closing);
+        this._setCursor(selStart + 1);
+      }
+    }
+  }
+
+  private _handleAutoSkip(e: KeyboardEvent): void {
+    this.elTextarea = this._getTextAreaEle();
+    const selStart = this.elTextarea.selectionStart;
+
+    if (this.code[selStart] === e.key) {
+      e.preventDefault();
+      this._setCursor(selStart + 1);
+    }
+  }
+
+  private _handleNewLine(e: KeyboardEvent) {
+    e.preventDefault();
+    this.elTextarea = this._getTextAreaEle();
+    this._insertCode(
+      this.elTextarea.selectionStart,
+      `\n${this._getCurrentLineIndent()}`
+    );
+  }
+
+  private _handleKeys(e: KeyboardEvent) {
+    switch (e.code) {
+      case "Tab":
+        this._handleTabs(e);
+        break;
+      case "Enter":
+        this._handleNewLine(e);
+        break;
+      case "Backspace":
+        this._handleBackspace(e);
+        break;
+      default:
+        if (this.opening.includes(e.key)) this._handleAutoClose(e);
+        else if (this.closing.includes(e.key)) this._handleAutoSkip(e);
+    }
   }
 
   render() {
-    const {
-      container,
-      header,
-      code,
-      badgeBase,
-      copyButtonButton,
-      copyButtonCopyIcon,
-      copyButtonSuccessIcon,
-      copyButtonErrorIcon,
-      copyButtonTooltipBase,
-      copyButtonTooltipBasePopup,
-      copyButtonTooltipBaseArrow,
-      copyButtonTooltipBody,
-    } = this.cssParts;
     return html`
-      <div part=${container} class=${container}>
-        <div part=${header} class=${header}>
-          <sl-badge
-            exportparts="badge-base: ${badgeBase}"
-            variant=${this.error ? "danger" : "primary"}
-          >
-            ${this.language}
-          </sl-badge>
-          <sl-copy-button
-            exportparts="
-        					copy-button: ${copyButtonButton},
-        					copy-icon: ${copyButtonCopyIcon},
-        					copy-success-icon: ${copyButtonSuccessIcon},
-        					copy-error-icon: ${copyButtonErrorIcon},
-        					tooltip__base: ${copyButtonTooltipBase},
-        					copy-tooltip__base__popup: ${copyButtonTooltipBasePopup},
-        					copy-tooltip__base__arrow: ${copyButtonTooltipBaseArrow},
-        					copy-tooltip__body: ${copyButtonTooltipBody}"
-            value=${this.value}
-            error-label="Nothing to copy!"
-          ></sl-copy-button>
+      <sl-textarea
+        @keydown=${this._handleKeys}
+        @input=${this._handleInput}
+        value=${this.code}
+        resize="auto"
+      >
+        <div id="label" slot="label">
+          ${this.label}
+          <sl-badge>${this.language}</sl-badge>
+          <sl-copy-button></sl-copy-button>
         </div>
-        <!-- TODO: add code that can be passed as attr -->
-        <lit-code
-          id=${code}
-          part=${code}
-          class=${this.error ? "error" : ""}
-          ?linenumbers=${this.lnu}
-          noshadow
-          language=${this.language}
-          @update=${() => this._setCode()}
-        >
-        </lit-code>
-      </div>
+      </sl-textarea>
     `;
   }
 }
