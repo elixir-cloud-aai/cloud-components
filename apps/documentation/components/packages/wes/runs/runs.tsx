0ff96c06543ed3790c8e4608ef967fd215bd5b31
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2024-04-15 12:13:37 +0530

    feat(docs): `Nextra` docs (#262)

---
+++ apps/documentation/components/packages/wes/runs/runs.tsx
@@ -0,0 +1,29 @@
+import dynamic from 'next/dynamic';
+import SlSkeleton from '@shoelace-style/shoelace/dist/react/skeleton';
+
+const ECCClientGa4ghWesRuns = dynamic(() => import('@elixir-cloud/wes/dist/react/runs/index'), {
+  ssr: false,
+  loading: () => (
+    <div className='flex flex-col gap-4'>
+      <div className='flex justify-end'>
+        <SlSkeleton effect='pulse' className='h-10 w-24 flex-row-reverse' />
+      </div>
+      <SlSkeleton effect='pulse' className='h-10' />
+      <SlSkeleton effect='pulse' className='h-10' />
+      <SlSkeleton effect='pulse' className='h-10' />
+      <SlSkeleton effect='pulse' className='h-10' />
+      <SlSkeleton effect='pulse' className='h-10' />
+      <div className='flex justify-center'>
+        <SlSkeleton effect='pulse' className='h-10 w-16' />
+      </div>
+    </div>
+  ),
+});
+
+export default function Runs() {
+  return (
+    <div>
+      <ECCClientGa4ghWesRuns />
+    </div>
+  );
+}
---
+++ apps/documentation/theme.config.tsx
@@ -0,0 +1,32 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import Image from 'next/image';
+import { FaSlack, FaGithub } from 'react-icons/fa';
+import Footer from './components/layout/footer';
+
+const config: DocsThemeConfig = {
+  logo: (
+    <>
+      <Image src='/elixir-cloud/logo.svg' alt='Logo' width={50} height={50} />
+      <span className='bg-red-400'>Elixir Cloud Components</span>
+    </>
+  ),
+  project: {
+    link: 'https://github.com/elixir-cloud-aai/cloud-components/',
+    icon: <FaGithub size={25} />,
+  },
+  chat: {
+    link: 'https://join.slack.com/t/elixir-cloud/shared_invite/enQtNzA3NTQ5Mzg2NjQ3LTZjZGI1OGQ5ZTRiOTRkY2ExMGUxNmQyODAxMDdjM2EyZDQ1YWM0ZGFjOTJhNzg5NjE0YmJiZTZhZDVhOWE4MWM',
+    icon: <FaSlack size={25} />,
+  },
+  docsRepositoryBase: 'https://github.com/elixir-cloud-aai/cloud-components',
+  footer: {
+    component: <Footer />,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Elixir Cloud Components',
+    };
+  },
+};
+
+export default config;
