8b0b6bfcbb10de8b47efe4fb7fd67a1c1772cf52
Anurag Gupta <me@anuragxd.com>
2024-07-16 13:09:31 +0530

    fix: wes url

--- apps/documentation/tailwind.config.js
+++ apps/documentation/tailwind.config.js
@@ -2,40 +2,7 @@
 module.exports = {
   darkMode: 'class',
   content: ['./pages/**/*.{js,jsx,ts,tsx,md,mdx}', './components/**/*.{js,jsx,ts,tsx,md,mdx}'],
-  theme: {
-    extend: {
-      animation: {
-        'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
-        slide: 'slide var(--speed) ease-in-out infinite alternate',
-      },
-      keyframes: {
-        'spin-around': {
-          '0%': {
-            transform: 'translateZ(0) rotate(0)',
-          },
-          '15%, 35%': {
-            transform: 'translateZ(0) rotate(90deg)',
-          },
-          '65%, 85%': {
-            transform: 'translateZ(0) rotate(270deg)',
-          },
-          '100%': {
-            transform: 'translateZ(0) rotate(360deg)',
-          },
-        },
-        slide: {
-          to: {
-            transform: 'translate(calc(100cqw - 100%), 0)',
-          },
-        },
-      },
-    },
-  },
-  safelist : [
-    'tracking-wide',
-    'font-semibold',
-    'gap-3',
-    'text-zinc-200'
-  ],
+  theme: {},
+  safelist: ['tracking-wide', 'font-semibold', 'gap-3', 'text-zinc-200'],
   plugins: [],
 };
