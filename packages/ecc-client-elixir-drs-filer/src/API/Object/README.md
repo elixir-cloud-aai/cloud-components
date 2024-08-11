0f2d4e8952b4ae7e29323944d9b749b8ca90bce9
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-08-12 01:08:10 +0530

    added delete endpoint
    Signed-off-by: Pratiksha Sankhe <sankhepratiksha3@gmail.com>

--- packages/ecc-client-elixir-drs-filer/src/API/Object/README.md
+++ packages/ecc-client-elixir-drs-filer/src/API/Object/README.md
@@ -14,6 +14,17 @@ Posts/Create a new object.
 |            |        | This should be an object containing the necessary fields for the object. |
 |            |        | Modify the structure according to your object requirements.              |
 
+## deleteObject
+
+Deletes a specific drs object.
+
+**API Request Body**
+
+| Parameter | Type   | Description                    |
+| --------- | ------ | ------------------------------ |
+| `baseURL` | string | Base URL for deleting object   |
+| `id`      | string | ID of the object to be deleted |
+
 
 ![Logo]('./../../../../images/logo-elixir.svg)
 ![Logo]('./../../../../images/logo-elixir-cloud-aai.svg)
--- packages/ecc-client-elixir-drs-filer/src/API/Object/drsAPI.ts
+++ packages/ecc-client-elixir-drs-filer/src/API/Object/drsAPI.ts
@@ -1,3 +1,11 @@
+/**
+ * Posts a new drs object.
+ * @param {string} baseURL - The base URL for posting the drs object.
+ * @param {object} taskData - The data of the drs object to be posted.
+ *                           This should be an object containing the necessary fields for the drs object.
+ *                           Modify the structure according to your object requirements.
+ * @returns {string} - A string that denotes the object id
+ */
 const postObject = async (baseURL: string, objectData: object) => {
   const url = `${baseURL}/objects`;
 
@@ -28,4 +36,24 @@ const postObject = async (baseURL: string, objectData: object) => {
   }
 };
 
-export { postObject };
+/**
+ *This mathod deletes a specific object
+ * @param id ID of the drs obeject to be deleted
+ */
+const deleteObject = async (baseURL: string, id: string) => {
+  const url = `${baseURL}/objects/${id}`;
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+    return response;
+  } catch (error) {
+    return {
+      isError: true,
+      breakpoint: "deleteObject",
+      error,
+    };
+  }
+};
+
+export { postObject, deleteObject };
