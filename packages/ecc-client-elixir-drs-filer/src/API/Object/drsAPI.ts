b38957d5c7db03d380cb7708a0ad30bba042095e
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-08-13 23:26:43 +0530

    feat: add delete endpoint (#345)
    Signed-off-by: Pratiksha Sankhe <sankhepratiksha3@gmail.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- packages/ecc-client-elixir-drs-filer/src/API/Object/drsAPI.ts
+++ packages/ecc-client-elixir-drs-filer/src/API/Object/drsAPI.ts
@@ -1,3 +1,11 @@
+/**
+ * Posts a new drs object.
+ * @param {string} baseURL - The base URL for posting the drs object.
+ * @param {object} objectData - The data of the drs object to be posted.
+ *                           This should be an object containing the necessary fields for the drs object.
+ *                           Modify the structure according to your object requirements.
+ * @returns {string} - A string that denotes the object id
+ */
 const postObject = async (baseURL: string, objectData: object) => {
   const url = `${baseURL}/objects`;
 
@@ -28,4 +36,33 @@ const postObject = async (baseURL: string, objectData: object) => {
   }
 };
 
-export { postObject };
+/**
+ *This method deletes a specific object
+ * @param id ID of the drs obeject to be deleted
+ */
+const deleteObject = async (baseURL: string, id: string) => {
+  const url = `${baseURL}/objects/${id}`;
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+
+    if (!response) {
+      return {
+        isError: true,
+        breakpoint: "deleteObject",
+        error: "No response from server",
+      };
+    }
+
+    return await response.json();
+  } catch (error) {
+    return {
+      isError: true,
+      breakpoint: "deleteObject",
+      error,
+    };
+  }
+};
+
+export { postObject, deleteObject };
