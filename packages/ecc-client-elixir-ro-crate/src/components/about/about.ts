52f6602024e7081cc19cfd7e963ad4796cce4b9c
Shivang <shivangtips@gmail.com>
2024-10-09 10:46:55 +0530

    Added fields corresponding to licence type

--- packages/ecc-client-elixir-ro-crate/src/components/about/about.ts
+++ packages/ecc-client-elixir-ro-crate/src/components/about/about.ts
@@ -7,12 +7,92 @@ export default class ECCClientRoCrateAbout extends LitElement {
   @state()
   private activeTab = 0;
 
-  // Method to switch tabs
-  private _switchTab(index: number) {
+  private _switchTab(index: number): void {
     this.activeTab = index;
   }
+  private _handleDataset(e: CustomEvent): void {
+    const licenceFieldIndex = this.AboutFields.findIndex(
+      (field) => field.key === "licence"
+    );
+    const licenceChildren = this.AboutFields[licenceFieldIndex].children ?? [];
+    if (licenceFieldIndex === -1) return;
+    let updatedChildren: Array<Field> = [];
 
-  static AboutFields: Field[] = [
+    if (e.detail.key === "licence" && e.detail.value === "URL") {
+      updatedChildren = licenceChildren
+        .filter((child) => child.key !== "@id")
+        .filter((child) => child.key !== "@type")
+        .filter((child) => child.key !== "name")
+        .filter((child) => child.key !== "desc");
+
+      const hasUrl = updatedChildren.some((child) => child.key === "url");
+      if (!hasUrl) {
+        updatedChildren.push({
+          key: "url",
+          label: "URL",
+          type: "url",
+          fieldOptions: {
+            required: true,
+          },
+        });
+      }
+    } else if (
+      e.detail.key === "licence" &&
+      e.detail.value === "CreativeWork"
+    ) {
+      updatedChildren = licenceChildren.filter((child) => child.key !== "url");
+
+      updatedChildren.push(
+        {
+          key: "@id",
+          label: "@id",
+          type: "text",
+          fieldOptions: {
+            required: true,
+            tooltip: "Persistent, managed unique ID in URL format",
+          },
+        },
+        {
+          key: "@type",
+          label: "@type",
+          type: "text",
+          fieldOptions: {
+            default: "CreativeWork",
+            required: true,
+            tooltip: "The type of the entity.",
+          },
+        },
+        {
+          key: "name",
+          label: "Name",
+          type: "text",
+          fieldOptions: {
+            required: true,
+            tooltip: "The name of the item.",
+          },
+        },
+        {
+          key: "desc",
+          label: "Description",
+          type: "text",
+          fieldOptions: {
+            required: true,
+            tooltip: "A description of the item.",
+          },
+        }
+      );
+    }
+    this.AboutFields = [
+      ...this.AboutFields.slice(0, licenceFieldIndex),
+      {
+        ...this.AboutFields[licenceFieldIndex],
+        children: updatedChildren,
+      },
+      ...this.AboutFields.slice(licenceFieldIndex + 1),
+    ];
+  }
+  @state()
+  AboutFields: Field[] = [
     {
       key: "@id",
       label: "@id",
@@ -79,13 +159,26 @@ export default class ECCClientRoCrateAbout extends LitElement {
     {
       key: "licence",
       label: "Licence",
-      type: "select",
+      type: "group",
       fieldOptions: {
         required: true,
       },
-      selectOptions: [
-        { label: "URL", value: "URL" },
-        { label: "Creative Work", value: "Creative Work" },
+      groupOptions: {
+        collapsible: true,
+      },
+      children: [
+        {
+          key: "licence",
+          label: "Licence Type",
+          type: "select",
+          fieldOptions: {
+            required: true,
+          },
+          selectOptions: [
+            { label: "URL", value: "URL" },
+            { label: "CreativeWork", value: "CreativeWork" },
+          ],
+        },
       ],
     },
   ];
@@ -367,7 +460,10 @@ export default class ECCClientRoCrateAbout extends LitElement {
       <div class="content">
         ${this.activeTab === 0
           ? html`<ecc-utils-design-form
-              .fields=${ECCClientRoCrateAbout.AboutFields}
+              @ecc-utils-change=${(e: CustomEvent) => {
+                this._handleDataset(e);
+              }}
+              .fields=${this.AboutFields}
             />`
           : ""}
         ${this.activeTab === 1
